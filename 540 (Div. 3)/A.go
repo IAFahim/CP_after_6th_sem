e7c1204c2fa1c97ea71dc6009a3a14d1bd9f2cd4
fahimmanowarj5 <fahimmanowarj5@gmail.com>
2022-02-20 14:08:51 +0600

    4 wow but took 1:30h

---
+++ 540 (Div. 3)/A.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func (in *A) run() {
+	q := in.nextInt()
+	for i := 0; i < q; i++ {
+		n, a, b := in.nextInt64(), in.nextInt64(), in.nextInt64()
+		evenRate := float64(b) / 2.0
+		if float64(a) < evenRate {
+			fmt.Println(n * a)
+		} else {
+			var sum int64
+			if n%2 == 1 {
+				sum = a
+			}
+			fmt.Println(((n / 2) * b) + sum)
+		}
+	}
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	var F *bufio.Reader
+	if err == nil {
+		F = bufio.NewReader(file)
+		os.Stdin = file
+	} else {
+		F = bufio.NewReader(os.Stdin)
+	}
+	defer func(file *os.File) {
+		_ = file.Close()
+	}(file)
+	NewA(F).run()
+}
+
+func NewA(r *bufio.Reader) *A {
+	return &A{
+		sc:        r,
+		split:     []string{},
+		index:     0,
+		separator: " ",
+	}
+}
+
+type A struct {
+	sc        *bufio.Reader
+	split     []string
+	index     int
+	separator string
+}
+
+func (in *A) GetLine() string {
+	line, err := in.sc.ReadString('\n')
+	if err != nil {
+		fmt.Println("error line:", line, " err: ", err)
+	}
+	in.split = []string{}
+	in.index = 0
+	return line
+}
+
+func (in *A) load() {
+	if len(in.split) <= in.index {
+		in.split = strings.Split(in.GetLine(), in.separator)
+		in.index = 0
+	}
+}
+
+func (in *A) nextInt() int {
+	in.load()
+	val, _ := strconv.Atoi(strings.TrimSpace(in.split[in.index]))
+	in.index++
+	return val
+}
+
+func (in *A) nextInt64() int64 {
+	in.load()
+	val, _ := strconv.ParseInt(strings.TrimSpace(in.split[in.index]), 10, 64)
+	in.index++
+	return val
+}
+
+func (in *A) nextString() string {
+	in.load()
+	val := strings.TrimSpace(in.split[in.index])
+	in.index++
+	return val
+}
