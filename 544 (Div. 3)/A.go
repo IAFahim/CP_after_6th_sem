7ad075a6948bdef2191643029dc16f08cef77f2a
fahimmanowarj5 <fahimmanowarj5@gmail.com>
2022-02-24 22:02:02 +0600

    1 only why me

---
+++ 544 (Div. 3)/A.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func (in *A) run() {
+	ah, am := in.nextInt(), in.nextInt()
+	bh, bm := in.nextInt(), in.nextInt()
+	h := (bh + ah) * 3600
+	m := (bm + am) * 60
+	t := (h + m) / 2
+	H := t / 3600
+	T := (t % 3600) / 60
+	fmt.Print(time.Date(0, 0, 0, H, T, 0, 0, time.UTC).Format("15:04"))
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	var F *bufio.Reader
+	if err == nil {
+		F = bufio.NewReader(file)
+		os.Stdin = file
+	} else {
+		F = bufio.NewReader(os.Stdin)
+	}
+	defer func(file *os.File) {
+		_ = file.Close()
+	}(file)
+	NewA(F).run()
+}
+
+func NewA(r *bufio.Reader) *A {
+	return &A{
+		sc:        r,
+		split:     []string{},
+		index:     0,
+		separator: ":",
+	}
+}
+
+type A struct {
+	sc        *bufio.Reader
+	split     []string
+	index     int
+	separator string
+}
+
+func (in *A) GetLine() string {
+	line, err := in.sc.ReadString('\n')
+	if err != nil {
+		fmt.Println("error line:", line, " err: ", err)
+	}
+	in.split = []string{}
+	in.index = 0
+	return line
+}
+
+func (in *A) load() {
+	if len(in.split) <= in.index {
+		in.split = strings.Split(in.GetLine(), in.separator)
+		in.index = 0
+	}
+}
+
+func (in *A) nextInt() int {
+	in.load()
+	val, _ := strconv.Atoi(strings.TrimSpace(in.split[in.index]))
+	in.index++
+	return val
+}
+
+func (in *A) nextInt64() int64 {
+	in.load()
+	val, _ := strconv.ParseInt(strings.TrimSpace(in.split[in.index]), 10, 64)
+	in.index++
+	return val
+}
+
+func (in *A) nextString() string {
+	in.load()
+	val := strings.TrimSpace(in.split[in.index])
+	in.index++
+	return val
+}
